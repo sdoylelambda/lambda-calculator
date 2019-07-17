7eb4dbf0bc763e6dea2899e4ddc072fd4322ffca
Sean Doyle <sdoylelambda@gmail.com>
2019-07-16 21:45:47 -0400

    Number buttons and math buttons rendering.

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
+
+  const { operator } = props
+
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button value={operator.value}>{operator.char}</button>
     </>
   );
 };
+
+export default OperatorButton;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,18 @@
-import React from "react";
-
-//import any components needed
-
-//Import your array data to from the provided data file
+import React, {useState} from "react";
+import OperatorButton from '../OperatorButtons/OperatorButton';
+import { operators } from '../../../data';
 
 const Operators = () => {
-  // STEP 2 - add the imported data to state
+  
+  const [operator, setOperator] = useState(operators)
+
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       {operator.map((operator, id) => (
+         <OperatorButton key={id} operator={operator}  />
+       ))}
     </div>
   );
 };
+
+export default Operators;
